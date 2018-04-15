19ffa1e9db35d568723a03cf285891c0c0582f7b
Pietro Verga <devpit@pitfactory.it>
2018-04-15 12:19:34 +0200

    Fixes

--- example.php
+++ example.php
@@ -2,7 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-$session = new \devpit\ScrapPage\scrap('https://www.t.me/Pit_Official', true); // class initialization
+$session = new \devpit\ScrapPage\Scrap('https://www.t.me/Pit_Official', true); // class initialization
 $session->setFirstPoint('<meta property="og:description" content="'); // setting first point
 $session->setSecondPoint('">'); //setting second point
 echo $session->scrapSite();
