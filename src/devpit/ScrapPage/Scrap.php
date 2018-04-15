4da9e9e1a5f762565257762cbf39bbcd83c9f95a
Pietro Verga <devpit@pitfactory.it>
2018-04-15 12:10:07 +0200

    Fixes and Updates

--- src/devpit/ScrapPage/Scrap.php
+++ src/devpit/ScrapPage/Scrap.php
@@ -8,53 +8,51 @@ class scrap
     public $fpoint;
     public $spoint;
     public $postaction;
-
-    public function __construct($site)
+    public function __construct($site, $check = false)
     {
-        $checksite = file_get_contents($site);
-        if ($checksite) {
+        if($check == true) {
+            $checksite = @file_get_contents($site);
+            if ($checksite) {
             $this->site = $site;
-
-            return 'Class initialized';
+            return true;
+           } else {
+            throw new Exception('Invalid site');
+           }
         } else {
-            exit('Error: site non valid');
+            return true;
         }
     }
-
     public function setFirstPoint($fpoint)
     {
         $this->fpoint = $fpoint;
-
-        return 'First point to scrap assigned';
+        return true;
     }
-
     public function setSecondPoint($spoint)
     {
         $this->spoint = $spoint;
-
-        return 'Second point to scrap assigned';
+        return true;
     }
-
     public function scrapSite()
     {
         if ($this->fpoint and $this->spoint) {
-            $ch = file_get_contents($this->site);
-            $pos1 = strpos($ch, $this->fpoint);
-            $pos2 = strpos($ch, $this->spoint, $pos1);
-            $get = substr($ch, $pos1, $pos2 - $pos1);
-
-            return $get;
+            $ch = @file_get_contents($this->site);
+            if($ch) {
+                $pos1 = strpos($ch, $this->fpoint);
+                $pos2 = strpos($ch, $this->spoint, $pos1);
+                $get = substr($ch, $pos1, $pos2 - $pos1);
+                return $get;
+            } else {
+                throw new Exception('Invalid site');
+            }
         } else {
-            exit('Error: point not assigned');
+            throw new Exception('Point not assigned');
         }
     }
-
     public function postAction($post)
     {
         $post = json_encode($post);
         $this->postaction = $post;
     }
-
     public function resultPostAction()
     {
         $jpostaction = json_decode($this->postaction);
@@ -70,7 +68,6 @@ class scrap
         curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
         curl_exec($ch);
         curl_close($ch);
-
         return $ch;
     }
 }
